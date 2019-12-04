1e42cf17c632c430e8e1c25bd3b21725ac662276
vinayak Mandrekar <57910687+webofvinayak@users.noreply.github.com>
2019-12-04 22:30:06 +0530

    0.0.2 keeper App Arrow function
    implemented function as Arrow function. dynamically inputs from javascripts objects list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => (
+        <Note title={note.title} content={note.content} />
+      ))}
 
       <Footer />
     </div>
